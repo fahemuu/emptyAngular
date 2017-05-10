806fc14873fcce1e922aab154521805837caf965
fahemuu <fahem.id@gmail.com>
2017-05-10 23:56:51 +0200

    git init et solution final , rien que ça

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,4 +1,33 @@
 // MAIN CONTROLLER
 function mainController($scope) {
-    
+    $scope.rows = []
+    $scope.m_fonction = function () {
+        $scope.$watch('form.$valid', function (valid) {
+
+
+
+                $scope.rows.push({
+                    Nom: $scope.Nom,
+                    Email: $scope.Email,
+                    Prenom: $scope.Prenom,
+
+                });
+                // console.log("avant",$scope.rows)
+                // $scope.rows = $scope.rows.filter((v, i, a) => a.indexOf(v) === i)
+                // console.log("apres",$scope.rows)
+
+
+
+            }
+
+        );
+
+
+        // console.log($scope.rows.Email)
+
+    }
+
+
 }
+
+ 
